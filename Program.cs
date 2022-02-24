1cca3e8f32dff104f35e75c3ccbd1f63df767fe1
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-02-24 10:44:38 -0300

    program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Aeds3TP1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello world");
+    }
+  }
+}
